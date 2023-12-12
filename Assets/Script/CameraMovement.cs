c5b101da8708d1ebd0b990786577f0fb67cfa2c7
Bilou4000 <153065643+Bilou4000@users.noreply.github.com>
2023-12-12 17:05:39 +0100

    [chore] new camera

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private Vector2 minPosition;
-    [SerializeField] private Vector2 maxPosition;
-    [SerializeField] private float smoothingSpeedx;
-    [SerializeField] private float smoothingSpeedy;
-    [SerializeField] private Camera cam;
     [SerializeField] private GameObject target;
-    private float x, y, z;
     [SerializeField] private float yOffset ;
     [SerializeField] private float xOffset ;
+    [SerializeField] private float smoothSpeed;
+
+    private float x, y, z;
+    private Vector3 velocity = Vector3.zero;
+
 
 
     void Start()
@@ -38,32 +37,38 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
-        if(target = PlayerMovement.Instance.gameObject)
-        {
-            xOffset = (target.GetComponent<Rigidbody2D>().velocity.x / 2);
-        }
+        y = target.transform.position.y + yOffset;
+        x = target.transform.position.x + xOffset;
 
-        if (target.transform.position.x < minPosition.x || target.transform.position.x > maxPosition.x)
-        {
-            x = Mathf.Clamp(target.transform.position.x, minPosition.x, maxPosition.x);
-        }
-        else if (Mathf.Abs(transform.position.x - target.transform.position.x) > 0.1f)
-        {
-            x = target.transform.position.x + xOffset;
-        }
-        if (target.transform.position.y < minPosition.y || target.transform.position.y > maxPosition.y)
-        {
-            y = Mathf.Clamp(target.transform.position.y + yOffset, minPosition.y, maxPosition.y);
-        }
-        else if (Mathf.Abs(transform.position.y - target.transform.position.y) > 0.1f)
-        {
-            y = target.transform.position.y + yOffset;
-        }
-        float targetPositionX = Mathf.Lerp(transform.position.x, x, smoothingSpeedx * Time.deltaTime);
-        float targetPositionY = Mathf.Lerp(transform.position.y, y, smoothingSpeedy * Time.deltaTime);
-        transform.position = new Vector3(targetPositionX, targetPositionY, z);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(x, y, 0), ref velocity, smoothSpeed);
 
-        
+
+        //if (target = PlayerMovement.Instance.gameObject)
+        //{
+        //    xoffset = (target.getcomponent<rigidbody2d>().velocity.x / 2);
+        //}
+
+        //if (target.transform.position.x < minposition.x || target.transform.position.x > maxposition.x)
+        //{
+        //    x = mathf.clamp(target.transform.position.x, minposition.x, maxposition.x);
+        //}
+        //else if (mathf.abs(transform.position.x - target.transform.position.x) > 0.1f)
+        //{
+        //    x = target.transform.position.x + xoffset;
+        //}
+        //if (target.transform.position.y < minposition.y || target.transform.position.y > maxposition.y)
+        //{
+        //    y = mathf.clamp(target.transform.position.y + yoffset, minposition.y, maxposition.y);
+        //}
+        //else if (mathf.abs(transform.position.y - target.transform.position.y) > 0.1f)
+        //{
+        //    y = target.transform.position.y + yoffset;
+        //}
+        //float targetpositionx = mathf.lerp(transform.position.x, x, smoothingspeedx * time.deltatime);
+        //float targetpositiony = mathf.lerp(transform.position.y, y, smoothingspeedy * time.deltatime);
+        //transform.position = new vector3(targetpositionx, targetpositiony, z);
+
+
 
     }
 
