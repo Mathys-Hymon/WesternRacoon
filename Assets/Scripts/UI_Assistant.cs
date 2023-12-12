124132a166954749c0b2800124860179d28b6ed0
JiniferSin <jane.saltsina@artfx.fr>
2023-12-12 17:18:46 +0100

    Action Text part4

--- Assets/Scripts/UI_Assistant.cs
+++ Assets/Scripts/UI_Assistant.cs
@@ -4,20 +4,33 @@ using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using CodeMonkey.Utils;
 
 public class UI_Assistant : MonoBehaviour
 {
     private TextMeshProUGUI messageTMP;
-    void Awake()
+    private void Awake()
     {
         messageTMP = transform.Find("message").Find("messageTMP").GetComponent<TextMeshProUGUI>();
 
-        transform.Find("message").GetComponent<Button>().ClickFunc = () => { };
+        transform.Find("message").GetComponent<Button_UI>().ClickFunc = () => 
+        { 
+            string[] messageArray = new string[] 
+            {
+                "HI",
+                "YO",
+                "HEHEHEHEHE",
+                "MUAHAHAHAHA",
+                "HEY",
+            }; 
+            string message = messageArray[Random.Range(0, messageArray.Length)];
+            TextWriter.AddWriter_Static(messageTMP, message, 0.05f);
+        };
     }
 
     private void Start()
     {
         //messageTMP.text = "Hello World!";
-        TextWriter.AddWriter_Static(messageTMP, "To be, or not to be, that is the question:\r\nWhether 'tis nobler in the mind to suffer\r\nThe slings and arrows of outrageous fortune...", .1f);
+        //TextWriter.AddWriter_Static(messageTMP, "To be, or not to be, that is the question:\r\nWhether 'tis nobler in the mind to suffer\r\nThe slings and arrows of outrageous fortune...", .1f);
     }
 }
