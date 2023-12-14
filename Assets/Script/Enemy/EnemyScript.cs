09b4c092e628f1b29f2eb4865ee12210a1cd1303
Bilou4000 <claire.benes@artfx.fr>
2023-12-14 14:27:00 +0100

    [feat] full bison shooting

--- Assets/Script/Enemy/EnemyScript.cs
+++ Assets/Script/Enemy/EnemyScript.cs
@@ -6,29 +6,61 @@ using UnityEngine;
 public class EnemyScript : FreezeMasterScript
 {
     [SerializeField] private GameObject bulletRef;
+    [SerializeField] private LayerMask obstacle;
     [SerializeField] private float delay;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating(nameof(Shoot),1f,delay);
-    }
+
+    private Vector2 directiontoTarget;
+
+    private bool canShoot = true;
+    private bool isInRange;
 
     // Update is called once per frame
     void Update()
     {
+        if (!freezed && isInRange)
+        {
+            float distance = Vector3.Distance(transform.position, PlayerMovement.Instance.transform.position);
+            RaycastHit2D touchPlayer =
+                Physics2D.Raycast(transform.position, (transform.position - PlayerMovement.Instance.transform.position)*(-1), distance, obstacle);
+            Debug.DrawRay(transform.position, (transform.position - PlayerMovement.Instance.transform.position)*(-1));
 
+            if (touchPlayer.collider == null && canShoot)
+            {
+                canShoot = false;
+                Shoot();
+            }
+        }
+       
     }
 
     public void Shoot()
     {
-        if (!freezed)
-        {
-            Vector2 DirectiontoTarget = PlayerMovement.Instance.transform.position - transform.position;
-            float angle = -90+Mathf.Atan2(DirectiontoTarget.y, DirectiontoTarget.x) * Mathf.Rad2Deg;
-            Instantiate(bulletRef, transform.position, Quaternion.Euler(0,0,angle));
-        }
+        directiontoTarget = PlayerMovement.Instance.transform.position - transform.position;
+        float angle = -90+Mathf.Atan2(directiontoTarget.y, directiontoTarget.x) * Mathf.Rad2Deg;
+        Instantiate(bulletRef, transform.position, Quaternion.Euler(0,0,angle));
+        
+        Invoke("ResetShoot",delay);
         
     }
-    
+
+    private void ResetShoot()
+    {
+        canShoot = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject == PlayerMovement.Instance.gameObject)
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == PlayerMovement.Instance.gameObject)
+        {
+            isInRange = false;
+        }
+    }
 }
