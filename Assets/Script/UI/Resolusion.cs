98d9dc86f349f6876319d1ab050c334b55137ad1
JiniferSin <jane.saltsina@artfx.fr>
2023-12-18 11:00:38 +0100

    [fix]  resolution dropdown
    now can be scrolled by joystick

---
+++ Assets/Script/UI/Resolusion.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Resolusion : MonoBehaviour
+{
+    Resolution[] resolutions;
+
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currrentResolutionIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currrentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currrentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    void Update()
+    {
+        
+    }
+}
