5c0be5286736ddcf22560554ec7c08cad36308ae
JiniferSin <jane.saltsina@artfx.fr>
2023-12-18 15:32:59 +0100

    [chore] coins movement

--- Assets/Script/GPE/Chest&Coins/CollectibleItem.cs
+++ Assets/Script/GPE/Chest&Coins/CollectibleItem.cs
@@ -7,12 +7,31 @@ public class CollectableItem : MonoBehaviour
 {
     [SerializeField] private Item itemData;
 
+    public float speed = 10f;
+    bool moveCoin;
+
+    GameObject target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("toCoins");
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             PlayerInventory.Instance.AddItemToInventory(itemData);
-            Destroy(gameObject);
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            moveCoin = true;
+            Destroy(gameObject, 5f);
+        }
+    }
+
+    void Update()
+    {
+        if(moveCoin)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
         }
     }
 }
