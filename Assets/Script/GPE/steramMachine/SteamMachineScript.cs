af3139e43ea2754e100f89e2aa90f2746cacc228
TortueFou <mathys.hymon@gmail.com>
2023-12-18 22:42:46 +0100

    [fix steam & moving plateforms

--- Assets/Script/GPE/steramMachine/SteamMachineScript.cs
+++ Assets/Script/GPE/steramMachine/SteamMachineScript.cs
@@ -8,6 +8,7 @@ public class SteamMachineScript : MonoBehaviour
     [Header("ObjectReferences")]
     [SerializeField] private ButtonScript[] buttons;
     [SerializeField] private ParticleSystem steamParticle;
+    [SerializeField] private LayerMask obstacle;
     [Header("Only for automatic Steam")]
     [SerializeField] private float pauseTime;
     [SerializeField] private float steamTime;
@@ -69,14 +70,39 @@ public class SteamMachineScript : MonoBehaviour
 
             if (IsValidInput == buttons.Length && collision.gameObject.GetComponent<BoxScript>() != null && collision.gameObject.GetComponent<Rigidbody2D>() != null)
             {
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = (transform.up * 10) / Vector3.Distance(transform.position, collision.transform.position);
-            }
+                float distance = Vector3.Distance(transform.position, collision.gameObject.transform.position);
+                RaycastHit2D touchPlayer = Physics2D.Raycast(transform.position, (transform.position - collision.gameObject.transform.position) * (-1), distance, obstacle);
+                Debug.DrawRay(transform.position, (transform.position - collision.gameObject.transform.position) * (-1));
+
+                if (touchPlayer.collider == null || touchPlayer.collider.gameObject.GetComponent<BoxScript>())
+                {
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = (transform.up * 10) / Vector3.Distance(transform.position, collision.transform.position);
+                }
+             }
         }
         else
         {
             if (pushCreate && collision.gameObject.GetComponent<BoxScript>() != null && collision.gameObject.GetComponent<Rigidbody2D>() != null)
             {
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = (transform.up * 10) / Vector3.Distance(transform.position, collision.transform.position);
+                float distance = Vector3.Distance(transform.position, collision.gameObject.transform.position);
+                RaycastHit2D touchPlayer = Physics2D.Raycast(transform.position + transform.up, (transform.position - collision.gameObject.transform.position) * (-1), distance, obstacle);
+                Debug.DrawRay(transform.position + transform.up, (transform.position - collision.gameObject.transform.position) * (-1));
+
+                if (touchPlayer.collider == null || touchPlayer.collider.gameObject.GetComponent<BoxScript>() != null)
+                {
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = (transform.up * 10) / Vector3.Distance(transform.position, collision.transform.position);
+                }
+            }
+        }
+        if (collision.gameObject == PlayerMovement.Instance.gameObject && pushCreate)
+        {
+            float distance = Vector3.Distance(transform.position, collision.gameObject.transform.position);
+            RaycastHit2D touchPlayer = Physics2D.Raycast(transform.position, (transform.position - collision.gameObject.transform.position) * (-1), distance, obstacle);
+            Debug.DrawRay(transform.position, (transform.position - collision.gameObject.transform.position) * (-1));
+
+            if (touchPlayer.collider == null)
+            {
+                PlayerMovement.Instance.Die();
             }
         }
     }
