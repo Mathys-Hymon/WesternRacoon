9afb12514b05e3ae9a3ebe8170b0ef9b2e41e2c6
JiniferSin <jane.saltsina@artfx.fr>
2023-12-18 11:47:52 +0100

    [add] collectable item

---
+++ Assets/Script/GPE/Chest&Coins/CollectibleItem.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour
+{
+    [SerializeField] private Item itemData;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInventory.Instance.AddItemToInventory(itemData);
+            Destroy(gameObject);
+        }
+    }
+}
+
+[Serializable]
+public class Item
+{
+    public string uniqueID;
+    public Sprite icon;
+}
