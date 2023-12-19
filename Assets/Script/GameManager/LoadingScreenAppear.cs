87d321b1fbadd91bf0a71d733692007222692075
JiniferSin <jane.saltsina@artfx.fr>
2023-12-19 12:05:24 +0100

    [chore] some anim fixes
    new collision script

--- Assets/Script/GameManager/LoadingScreenAppear.cs
+++ Assets/Script/GameManager/LoadingScreenAppear.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class LoadingScreenAppear : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        if (collision.gameObject == PlayerMovement.Instance.gameObject)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
