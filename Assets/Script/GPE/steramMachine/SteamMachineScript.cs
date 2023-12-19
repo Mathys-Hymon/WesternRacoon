de4af1e72924bcc89d63cde5cff674e6e1f51656
Rémy CHOLLET <remy.chollet@artfx.fr>
2023-12-19 14:03:46 +0100

    [Update] 19

--- Assets/Script/GPE/steramMachine/SteamMachineScript.cs
+++ Assets/Script/GPE/steramMachine/SteamMachineScript.cs
@@ -99,7 +99,6 @@ public class SteamMachineScript : MonoBehaviour
             float distance = Vector3.Distance(transform.position, collision.gameObject.transform.position);
             RaycastHit2D touchPlayer = Physics2D.Raycast(transform.position, (transform.position - collision.gameObject.transform.position) * (-1), distance, obstacle);
             Debug.DrawRay(transform.position, (transform.position - collision.gameObject.transform.position) * (-1));
-
             if (touchPlayer.collider == null)
             {
                 PlayerMovement.Instance.Die();
