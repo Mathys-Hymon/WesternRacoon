3bd1f1694b02002e2ad8abcabed1c456e5ece31d
Mathys Hymon <mathys.hymon@gmail.com>
2023-12-20 16:48:02 +0100

    [refraction] cannonSpawnOffset

--- Assets/Script/GPE/Traps/Cannon/CannonScript.cs
+++ Assets/Script/GPE/Traps/Cannon/CannonScript.cs
@@ -8,26 +8,35 @@ public class CannonScript : MonoBehaviour
     [SerializeField] private float shootFrequency;
     [SerializeField] private float bulletSpeed;
     [SerializeField] private bool startShooting;
+    [SerializeField] private float bulletSpawnOffset = 1f;
 
     [Header("BULLET REFERENCE")]
     [SerializeField] public GameObject bulletPrefab;
 
 
+    private SpriteRenderer sr;
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         if(startShooting)
         {
             ShootBullet();
         }
         else
         {
-            Invoke("ShootBullet", 10f / shootFrequency);
+            Invoke("BulletAnticipation", 8f / shootFrequency);
         }
         
     }
+    private void BulletAnticipation()
+    {
+        sr.color = Color.red;
+        Invoke("ShootBullet", 2f / shootFrequency);
+    }
     private void ShootBullet()
     {
-        GameObject bullet = Instantiate(bulletPrefab, transform.position + (transform.right * 2), transform.rotation);
+        sr.color = Color.clear;
+        GameObject bullet = Instantiate(bulletPrefab, transform.position + (transform.right * bulletSpawnOffset), transform.rotation);
         bullet.GetComponent<CannonBulletScript>().SetBulletSpeed(bulletSpeed);
         Invoke("ShootBullet", 10f / shootFrequency);
     }
