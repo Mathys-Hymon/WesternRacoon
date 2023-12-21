5edeca9c7b5ae8de0ce6a4c29cf6ddf411028e94
Bilou4000 <claire.benes@artfx.fr>
2023-12-21 15:48:25 +0100

    [wip] test

--- Assets/Script/SaveSystem/SaveSystem.cs
+++ Assets/Script/SaveSystem/SaveSystem.cs
@@ -23,10 +23,10 @@ public class SaveSystem : MonoBehaviour
             saveInfo = JsonUtility.FromJson<AllInfo>(json);
             
             PlayerMovement.Instance.transform.position = new Vector3(saveInfo.x, saveInfo.y, saveInfo.z);
-            GameManager.Instance.money = saveInfo.money;
-            
-            GameObject Chest1 = GameObject.Find("Chest01");
-            Chest1.GetComponent<ChestOpen>().isOpened = saveInfo.chest;
+            // GameManager.Instance.money = saveInfo.money;
+            //
+            // GameObject Chest1 = GameObject.Find("Chest01");
+            // Chest1.GetComponent<ChestOpen>().isOpened = saveInfo.chest;
             
             CameraScript.Instance.NewCameraBoundary(new Vector2(saveInfo.cameraPosX, saveInfo.cameraPosY));
             CameraScript.Instance.transform.position = new Vector3(saveInfo.x, CameraScript.Instance.transform.position.y, saveInfo.z);
@@ -43,19 +43,17 @@ public class SaveSystem : MonoBehaviour
         saveInfo.x = position.x;
         saveInfo.y = position.y;
         saveInfo.z = position.z;
-
-        int coins = GameManager.Instance.money;
-        saveInfo.money = coins;
-
+        
         Vector2 cameraPosition = CameraScript.Instance.GetBoundaries();
         saveInfo.cameraPosX = cameraPosition.x;
         saveInfo.cameraPosY = cameraPosition.y;
-        
-        
-        
-        GameObject Chest1 = GameObject.Find("Chest01");
-        bool chestOpen01 = Chest1.GetComponent<ChestOpen>().isOpened;
-        saveInfo.chest = chestOpen01;
+
+        // int coins = GameManager.Instance.money;
+        // saveInfo.money = coins;
+        //
+        // GameObject Chest1 = GameObject.Find("Chest01");
+        // bool chestOpen01 = Chest1.GetComponent<ChestOpen>().isOpened;
+        // saveInfo.chest = chestOpen01;
 
         Debug.Log(Application.persistentDataPath + "/data.save");
         string json = JsonUtility.ToJson(saveInfo);
