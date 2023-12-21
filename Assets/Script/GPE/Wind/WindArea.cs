afbd254d75bd834b8a869f3a9fb0998b0e1389a5
Mathys Hymon <mathys.hymon@gmail.com>
2023-12-21 11:43:37 +0100

    [fix] wind

--- Assets/Script/GPE/Wind/WindArea.cs
+++ Assets/Script/GPE/Wind/WindArea.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class WindArea : MonoBehaviour
 {
     [SerializeField] private float strength;
-    [SerializeField] private float XDirection;
+    [SerializeField] private bool GoLeft;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.gameObject == PlayerMovement.Instance.gameObject)
         {
-            PlayerMovement.Instance.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(XDirection, 0) *  strength);
+            if(GoLeft)
+            {
+                PlayerMovement.Instance.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, 0) * strength);
+            }
+            else
+            {
+                PlayerMovement.Instance.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 0) * strength);
+            }
+           
         }
     }
 }
